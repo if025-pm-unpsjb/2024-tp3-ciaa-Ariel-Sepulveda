06c44b2ad66ee8757d738d965b187a21395b4a50
Ariel Sepulveda <ariel.sepulveda.ar@gmail.com>
2024-12-03 16:20:02 -0300

    tp4-uart-led

--- apps/tp4-uart-led/src/app.c
+++ apps/tp4-uart-led/src/app.c
@@ -8,17 +8,13 @@
 #include "app.h"         // <= Su propia cabecera (opcional)
 #include "sapi.h"        // <= Biblioteca sAPI
 
-#define mainMAX_MSG_LEN 25
-
 int main( void )
 {
-		char cMessage[ mainMAX_MSG_LEN ];
 		gpioMap_t leds[] = {CIAA_BOARD_LED, LED1, LED2, LED3};
 		int idx;
 		uint8_t receivedByte;
 		bool_t input;
 		bool_t value;
-
 
 	    uartConfig( UART_USB, 9600);
 
